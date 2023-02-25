1c55379a13f3073c60a80fa391b2f2a191bff241
Alejandro Toledo <88223531+WernerToledo@users.noreply.github.com>
2023-02-25 14:42:40 -0600

    AGG, eliminar, actulizar, consultar

--- practica01/Controllers/AlumnoController.cs
+++ practica01/Controllers/AlumnoController.cs
@@ -26,5 +26,99 @@ namespace practica01.Controllers
             if (listadoAlumno.Count == 0) { return NotFound(); }
             return Ok(listadoAlumno);
         }
+
+        //      localhost/apis/bd/metodo
+        //url = localhost/api/equipos/getbyid?id=23&nombre=pwa
+        [HttpGet]
+        [Route("getbyid/{id}")]
+        
+        public IActionResult Get(int id) {
+            Alumno? alumno = (from e in _alumnoContext.Alumno where e.cod == id select e).FirstOrDefault();
+            if (alumno == null) return NotFound();
+            return Ok(alumno);
+        }
+
+        [HttpGet]
+        [Route("find")]
+
+        public IActionResult Buscar(String filtro)
+        {
+            //El contexto va a la base de datos
+
+            List<Alumno> alumno = (from e in _alumnoContext.Alumno where e.nombre == filtro select e).ToList();
+            //uso del Like
+            //List<Alumno> alumno = (from e in _alumnoContext.Alumno where e.nombre.Contains(filtro) select e).ToList();
+
+            //uso del or
+            //List<Alumno> alumno = (from e in _alumnoContext.Alumno where e.nombre.Contains(filtro) || e.apellido.Contains(filtro) select e).ToList();
+
+            //si existe algun registro
+            if (alumno.Any()) return Ok(alumno);
+            return NotFound();
+
+            //if (alumno.Count() == 0) return NotFound();
+            //return Ok(alumno);
+        }
+
+        //se usa comunmente con post por que ya viene habilitado
+        //put y otros verbos deben codificarse
+        [HttpPost]
+        [Route("add")]
+
+        //filosofia de sully
+        public IActionResult crear([FromBody] Alumno pAlumno)
+        {
+            try
+            {
+                _alumnoContext.Alumno.Add(pAlumno);
+                //es como el execute query
+                _alumnoContext.SaveChanges();
+
+                //se obtendran los datosd de la base de datos
+                //se puede solo obtener solo un elemento del objeto para que el tamaño del request sea menor
+                return Ok(pAlumno);
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        //Para modificar
+        [HttpPut]
+        [Route("Actulizar/{id}")]
+        public IActionResult actulizarAlumno(int id, [FromBody] Alumno pAlumno)
+        {
+            Alumno? alumno = (from e in _alumnoContext.Alumno where e.cod == id select e).FirstOrDefault();
+
+            if (alumno == null)
+                return NotFound();
+            
+
+            alumno.nombre = pAlumno.nombre;
+            alumno.apellido = pAlumno.apellido;
+
+            _alumnoContext.Entry(alumno).State = EntityState.Modified;
+            _alumnoContext.SaveChanges();
+
+            return Ok(alumno.nombre);
+        }
+
+        // no se recomienda borrar sino que se recomienda cambiar el estado
+        [HttpDelete]
+        [Route("Eliminar")]
+        public IActionResult eliminar(int id)
+        {
+            Alumno? alumno = (from e in _alumnoContext.Alumno where e.cod == id select e).FirstOrDefault();
+
+            if (alumno == null)
+                return NotFound();
+                
+
+            _alumnoContext.Alumno.Attach(alumno);
+            _alumnoContext.Alumno.Remove(alumno);
+            _alumnoContext.SaveChanges();
+
+            return Ok();
+        }
     }
 }
